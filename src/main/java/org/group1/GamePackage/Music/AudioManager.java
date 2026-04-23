8b58ff84f2fcda119379250107f3d5681df549a0
WDCT-Wren <wrenctulio@gmail.com>
2026-04-23 23:09:08 +0800

    feat: added options menu to pause menu that has its own audio control that controls global sound effects as well as music volume

--- src/main/java/org/group1/GamePackage/Music/AudioManager.java
+++ src/main/java/org/group1/GamePackage/Music/AudioManager.java
@@ -5,24 +5,34 @@ import com.almasb.fxgl.audio.Sound;
 import com.almasb.fxgl.dsl.FXGL;
 
 public class AudioManager {
+    private static double audioVolume = 20.0;
+    private static double musicVolume = 0.5;
 
+    public static void setGlobalSoundVolume(double newVolume) {
+        audioVolume = newVolume;
+        FXGL.getSettings().setGlobalSoundVolume(newVolume);
+    }
+    public static void setMusicVolume(double newVolume) {
+        musicVolume = newVolume;
+        FXGL.getSettings().setGlobalMusicVolume(newVolume);
+    }
 
     public void playBackgroundMusic() {
         Music music = FXGL.getAssetLoader().loadMusic("main_music.wav");
         FXGL.getAudioPlayer().loopMusic(music);
-        music.getAudio().setVolume(0.3);
+        music.getAudio().setVolume(musicVolume);
     }
 
     public void playDeathSound() {
         Sound sound = FXGL.getAssetLoader().loadSound("death_sound.wav");
         FXGL.getAudioPlayer().playSound(sound);
-        sound.getAudio().setVolume(20.0);
+        sound.getAudio().setVolume(audioVolume);
     }
 
     public void playHeartGain() {
         Sound sound = FXGL.getAssetLoader().loadSound("heart_gain.wav");
         FXGL.getAudioPlayer().playSound(sound);
-        sound.getAudio().setVolume(20.0);
+        sound.getAudio().setVolume(audioVolume);
     }
 
     public static void bossDie() {
@@ -51,4 +61,7 @@ public class AudioManager {
         FXGL.getAudioPlayer().stopAllSounds();
     }
 
+    //getters
+    public static double getMusicVolume() {return musicVolume;}
+    public static double getAudioVolume() {return audioVolume;}
 }
--- src/main/java/org/group1/GamePackage/UI/Controllers/PauseController.java
+++ src/main/java/org/group1/GamePackage/UI/Controllers/PauseController.java
@@ -1,11 +1,48 @@
 package org.group1.GamePackage.UI.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+import org.group1.GamePackage.Music.AudioManager;
 import org.group1.GamePackage.UI.PauseInterface;
 
 public class PauseController {
     private PauseInterface menu;
+    @FXML
+    private VBox audioMenu;
 
+    @FXML
+    private VBox optionsMenu;
+
+    @FXML
+    private VBox pauseMenu;
+
+    @FXML
+    private Slider soundVolumeSlider;
+
+    @FXML
+    private Slider musicVolumeSlider;
+
+    /**
+     * Initializes the PauseController by setting up the audio sliders to be able to
+     * change the music and sound effects volume
+     */
+    @FXML
+    private void initialize() {
+        // Set sliders to current values first
+        musicVolumeSlider.setValue(AudioManager.getMusicVolume());
+        soundVolumeSlider.setValue(AudioManager.getAudioVolume());
+
+        // Connect Music Slider to music volume
+        musicVolumeSlider.valueProperty().addListener((_, _, newVal) -> {
+            AudioManager.setMusicVolume(newVal.doubleValue());
+        });
+
+        // Connect Sound Slider to the sound effects
+        soundVolumeSlider.valueProperty().addListener((_, _, newVal) -> {
+            AudioManager.setGlobalSoundVolume(newVal.doubleValue());
+        });
+    }
     public void setMenu(PauseInterface menu) {
         this.menu = menu;
     }
@@ -15,6 +52,36 @@ public class PauseController {
      */
     @FXML
     private void onResume() { menu.resume(); }
+
+    /**
+     * switches the pause menu to the options menu scene
+     */
+    @FXML
+    private void onPauseMenu() {
+        optionsMenu.setVisible(false);
+        audioMenu.setVisible(false);
+        pauseMenu.setVisible(true);
+    }
+
+    /**
+     * switches the pause menu to the options menu scene
+     */
+    @FXML
+    private void onOptions() {
+        optionsMenu.setVisible(true);
+        audioMenu.setVisible(false);
+        pauseMenu.setVisible(false);
+    }
+
+    /**
+     * switches the options menu to the audio menu scene
+     */
+    @FXML
+    private void onAudio() {
+        audioMenu.setVisible(true);
+        optionsMenu.setVisible(false);
+        pauseMenu.setVisible(false);
+    }
 
     /**
      * Exits the game to the main menu
