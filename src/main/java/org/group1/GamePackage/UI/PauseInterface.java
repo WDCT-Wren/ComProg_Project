3642ff0f30d89b52eada63800ec54976001ccf76
WDCT-Wren <wrenctulio@gmail.com>
2026-04-30 23:18:43 +0800

    Fix: fixed bug where game music still plays after exiting to main menu

--- src/main/java/org/group1/GamePackage/UI/PauseInterface.java
+++ src/main/java/org/group1/GamePackage/UI/PauseInterface.java
@@ -4,9 +4,13 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import org.group1.GamePackage.Music.AudioManager;
 import org.group1.GamePackage.UI.Controllers.PauseController;
 
 import java.io.IOException;
+
+import static com.almasb.fxgl.dsl.FXGL.getDialogService;
+import static com.almasb.fxgl.dsl.FXGL.getGameController;
 
 public class PauseInterface extends FXGLMenu {
 
@@ -26,5 +30,22 @@ public class PauseInterface extends FXGLMenu {
 
     //getters for the buttons
     public void resume() { fireResume(); }
-    public void exitToMainMenu() { fireExitToMainMenu(); }
+
+    /**
+     * Invokes a confirmation dialog to confirm if the user really wants to exit the game
+     * <br><br>
+     * Basically overrides the default {@code exitToMainMenu} method in {@code FXGLMenu} that doesn't stop the music.
+     */
+    public void exitToMainMenu() {
+        getDialogService().showConfirmationBox("Are you sure you want to end the adventure early?", (Boolean answer) -> {
+            if (answer) {
+                // automatically goes to main menu when confirmed
+                getGameController().gotoMainMenu();
+
+                // Game music stops, and menu music plays
+                AudioManager.stopAll();
+                AudioManager.playMenuMusic();
+            }
+        });
+    }
 }
